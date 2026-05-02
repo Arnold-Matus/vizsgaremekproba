d7cf5dcf0a19518b4fbc88555e4a5fc68051814a
Arnold-Matus <67595573+Arnold-Matus@users.noreply.github.com>
2026-05-02 21:20:14 +0200

    Add files via upload

---
+++ storage/framework/views/ac7b8dd75cc7f58c891aadb7418c60cd.php
@@ -0,0 +1,8 @@
+<section
+    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</section>
+<?php /**PATH C:\Users\Win11Prem2026Marcius\Downloads\vizsgaremekproba-Arnold-Matus-patch-13\vizsgaremekproba-Arnold-Matus-patch-13\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/section-container.blade.php ENDPATH**/ ?>
